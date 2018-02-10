884d2083a378141a7b7d902d4bdb0569897e569a
Anna Vihrogonova <annavihrogonova@yahoo.com>
2018-02-10 16:57:33 +0000

    Create Geolocation class and check for geolocation support in browser. If it is support create method to  return promise with latitude and longitude

--- src/getGeolocation.js
+++ src/getGeolocation.js
@@ -1,50 +1,52 @@
 'use strict';
 
-export class GetGeolocation {
+//Importing classes
+import { WeatherUI } from './weatherUI';
+
+//Make instance of weatherUI
+const newUI = new WeatherUI();
+
+export class Geolocater {
+
     constructor() {
-        this.lat = 0;
-        this.long = 0;
-
-    }
-
-    //Get Geolocation
-    getGeolocation(){ 
-        console.log(this.lat); 
-        let options, geoloc, watchID;
-
-        if(navigator.geolocation) {
-
-            options = {timeout:60000};
-
-            geoloc = navigator.geolocation;
-
-            watchID = geoloc.watchPosition(this.weatherData,  this.errorCallback, options);
-            
-
+        //Check for browser support
+        if ( this.isGeolocationAvailable() ) {
+          //If it is support - create navigator
+          this.navigator = navigator;
+          //If no support display notice to user
         } else {
-            showError("Your browser does not support Geolocation!");
+          return this.isGeolocationNotAvailable();
         }
 
-       
-    }
+      }
 
-    //Getting weather data
-   weatherData(position){
-        let crd;
-        crd = position.coords;
-        this.lat  =  crd.latitude;
-        this.long =  crd.longitude; 
-        console.log(this.lat); 
-    }
+      //1.Checking for support 
+      isGeolocationAvailable() {
+        return 'geolocation' in navigator ? true : false;
+      }
 
-    //CallBack function if user block geolocation
-   errorCallback(error) {
-        if(error.code == error.PERMISSION_DENIED){
-            alert('Sorry, You have to allow to share your geolcation or use search by City name');
-        }
-    }
+      //2.Notice user if browser does not support geolocation
+      isGeolocationNotAvailable() {
+         alert('Sorry, your browser does not support Geolococatio');
+      }
 
+      //3.Getting coordinats from navigator 
+      watchPosition() {
+        let options  = {timeout:60000};
 
+        //Return promise 
+        return new Promise( ( resolve, reject, options ) => {
 
-
+          this.navigator.geolocation.watchPosition( ( position ) => {
+            
+            //Getting latitude and longitude and resolve them
+            let latitude  = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            
+            
+            resolve( { latitude, longitude } );
+          //If user does not allow to share geolocation notify them
+          }, () => { reject( alert('Sorry, you did not allow to share your geolocation, try to use search BY city name')); } );
+        });
+      }
 }
